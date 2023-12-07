8c50128202fd23088d03a50540e7f277ee709468
lightm103 <mchansws103@gmail.com>
2023-12-07 19:51:26 +0700

    .

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Invoice;
-use Barryvdh\DomPDF\Facade\PDF;
+use Barryvdh\DomPDF\PDF;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
